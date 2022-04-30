ec079bfc5335f6e4474d7568c965b4f64c8cc32a
Karynes16 <97715666+Karynes16@users.noreply.github.com>
2022-04-30 15:16:45 -0400

    commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+    INCLUDE("database.php");
+?>
+<html>
+  <head>
+  <link rel="stylesheet" href="stylesheet.css">
+    <title>Subscribers</title>
+  </head>
+  <body>
+   <h2>Current Podcast Subscribers</h2>
+    <?php
+       connect();
+       salesTable();
+       updateTable();
+       close();
+       ?>
+
+<form action="index.php" method="POST">
+<input type="number" name="customer_id"/>
+<input type="text" name="first_name"/>
+<input type="text" name="last_name"/>
+<input type="text" name="city"/>
+<input type="text" name="state"/>
+<input type="submit"/>
+</form>
+</body>
+</html>
